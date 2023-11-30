412e5827d5b6646333137d62113bc29521decc69
salkrid <bohner@yandex.ru>
2023-11-30 16:24:04 +0300

    first js file

---
+++ main.js
@@ -0,0 +1,135 @@
+// переменные
+// const b=150;
+// b=200;
+
+
+// document.write(a--);
+// document.write("<br>");
+// document.write(a);
+
+// let text = "11";
+// let a=10;
+// let b=a+parseInt(text);
+
+// document.write(typeof b)
+// document.write(a + text);
+
+// let a = "123";
+// let x=123;
+
+
+// document.write(x==a)
+// document.write("<br>")
+// document.write(x===a)
+
+// alert("message");
+// let userNumber = prompt("enter a number");
+// let num = 10;
+
+//Условие 
+
+// if (userNumber<100) {
+//     document.write("number less than 100")
+// } else {
+//     document.write("number more than 100")
+// }
+
+
+// Краткая запись условия 
+
+// let num = prompt("enter a number:")
+// let result = num % 2 == 0 ? "odd number" : "even number"
+// document.write(result);
+
+// let numForSwitch = 10;
+
+// switch (numForSwitch) {
+//     case 1:
+//         document.write('given number 1');
+//         break;
+//     case 5:
+//         document.write('given number 5');
+//         break;
+//     case 10:
+//         let b = numForSwitch + 100;
+//         document.write('given number' + b);
+//     default:
+//         console.log("no such number");
+
+// }
+
+// Операторы
+// && и логическое умножение 
+// || или логическое сложение
+// !
+
+// let result = false && true;
+// document.write(result);
+
+// let num2 = 15;
+
+// if (num2 < 30 || num2 > 20) {
+//     document.write("number is ok");
+// } else {
+//     document.write("number not ok");
+// }
+
+
+// ЦИКЛЫ 
+// WHILE 
+
+// let i = 5;
+
+// while(i<10){
+//     document.write(i);
+//     document.write("<br>");
+//     i++;
+// }
+
+// document.write ("we quit we cycle");
+
+// let k = 1;
+
+// do {
+//     document.write(k);
+//     document.write("<br>");
+//     k++;
+// }
+
+// while(k < 4);    
+
+
+// document.write ("we quit we cycle");
+
+
+// for (let j = 0; j < 10; j++) {
+//     document.write(j);
+//     document.write("<br>");
+//     if (j == 5) break;
+
+// }
+
+// for (let j = 0; j < 10; j++) {
+//     if (j%2 ==0) continue;
+//     document.write(j);
+//     document.write("<br>");
+    
+// }
+ 
+// let result = confirm ("you want to continue register?");
+// console.log(result);
+
+function printHello() {
+    document.write("Hi");
+    document.write("<br>");
+    document.write("how r u");
+    document.write("<br>");
+    document.write("whtas up");
+    document.write("<br>");
+}
+
+printHello();
+
+
+
+
